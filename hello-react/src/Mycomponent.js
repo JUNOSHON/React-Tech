69d0a951400ac4f80760e2a1609128d24b3e3f22
junoshon <sanoldfox@naver.com>
2023-03-09 00:59:42 +0900

    Mycomponent

--- hello-react/src/Mycomponent.js
+++ hello-react/src/Mycomponent.js
@@ -1,15 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {defaultKeyMap} from "@testing-library/user-event/dist/keyboard/keyMap";
+import { defaultKeyMap } from "@testing-library/user-event/dist/keyboard/keyMap";
 
-const MyComponent = ({name, favoriteNumber, children}) => {
-  
+const MyComponent = ({ name, favoriteNumber, children }) => {
   return (
     <div>
-      제 이름은 {name}입니다. <br/>
-      children 값은 {children}입니다.<br/>
+      제 이름은 {name}입니다. <br />
+      children 값은 {children}입니다.
+      <br />
       제가 좋아하는 숫자는 {favoriteNumber}입니다.
-    
     </div>
   );
 };
