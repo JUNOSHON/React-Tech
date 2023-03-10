957565e8f2a63f480e86130fa2419bfd76e904e0
junoshon <sanoldfox@naver.com>
2023-03-10 17:12:38 +0900

    styled-component

---
+++ styling-react/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import {Fragment} from "react";
+import SassComponent from "./SassComponent";
+import StyledComponent from "./StyleComponent";
+
+function App() {
+  return (
+    <Fragment>
+    <StyledComponent/>
+    </Fragment>
+  )
+    ;
+}
+
+export default App;
---
+++ styling-react/src/SassComponent.js
@@ -0,0 +1,22 @@
+import './SassComponent.scss'
+
+import React from 'react';
+
+export default function SassComponent() {
+    return (
+        <div className="SassComponent">
+          <div className="box red" />
+          <div className="box orange" />
+          <div className="box yellow" />
+          <div className="box green" />
+          <div className="box blue" />
+          <div className="box indigo" />
+          <div className="box violet" />
+          
+          
+          
+          
+          
+        </div>
+    )
+}
