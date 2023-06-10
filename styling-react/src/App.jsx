1a5ddc022ad62635d011e41d58e1623ca8d1968a
junoshon <sanoldfox@naver.com>
2023-06-10 14:17:50 +0900

    emotion

--- styling-react/src/App.jsx
+++ styling-react/src/App.jsx
@@ -1,16 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
 import {Fragment} from "react";
-import SassComponent from "./SassComponent";
-import StyledComponent from "./StyleComponent";
+import SassComponent from "./sample/SassComponent";
+import StyledComponent from "./sample/StyleComponent";
+import styled, {ThemeProvider} from "styled-components";
+import Button from "./components/Button";
+import Box from './components/Box'
+import {css,jsx} from '@emotion/react'
+
+const divStyle = css`
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  padding: 32px;
+  text-align: center;
+
+  &:hover {
+    color: white;
+    font-size: 50px
+  }
+
+`
+
+
 
 function App() {
   return (
-    <Fragment>
-    <StyledComponent/>
-    </Fragment>
-  )
-    ;
+    <div css={divStyle}>Hover to change Color</div>
+  );
 }
+
 
 export default App;
