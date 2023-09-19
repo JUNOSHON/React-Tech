0f59e32fd7871a171aefeb8a853dbfba7d50eb94
junoshon <sanoldfox@naver.com>
2023-09-19 22:32:29 +0900

    react-redux

--- react-redux-tutorial/src/components/Counter.jsx
+++ react-redux-tutorial/src/components/Counter.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useState} from "react";
 
-export default function Counter() {
+export default function Counter({number,onIncrease,onDecrease}) {
+  
     return (
-        <div>Counter</div>
-    )
-}
+      <div>
+        <h1>{number}</h1>
+        <div>
+          <button onClick={onIncrease}>+1</button>
+          <button onClick={onDecrease}>-1</button>
+        </div>
+      </div>
+    );
+  };
--- react-redux-tutorial/src/modules/index.js
+++ react-redux-tutorial/src/modules/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux';
 import counter from "./counter";
-import todos from "../components/Todos";
+// import todos from "../components/Todos";
+import todos from "./todos"
 
 const rootReducer = combineReducers({
   counter,
